0cceda898c6165be580d57083ac75d765e64f855
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-03-19 04:50:12 -0300

    Add router call

--- TodoList/Tasks/Controllers/TaskListController.swift
+++ TodoList/Tasks/Controllers/TaskListController.swift
@@ -4,12 +4,15 @@ import UIKit
 class TaskListController: UIViewController {
 
     private let taskListView: TaskListView
+    private var router: TasksRouter
 
-    init() {
+    init(router: TasksRouter) {
+        self.router = router
         taskListView = TaskListView(dataSource: TaskListDataSource(tasks: []))
         super.init(nibName: nil, bundle: nil)
         setSubviews()
         setConstraints()
+        setNavigation()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -24,4 +27,13 @@ class TaskListController: UIViewController {
         taskListView.edges(equalTo: view)
     }
 
+    private func setNavigation() {
+        let barButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTouchAtCreateTask))
+        navigationItem.setRightBarButton(barButton, animated: true)
+        title = String.Tasks.List.title
+    }
+
+    @objc private func didTouchAtCreateTask() {
+        router.create()
+    }
 }
