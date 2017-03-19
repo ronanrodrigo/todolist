e6239d5f874234ec093520e69fa04862f7b05956
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-03-19 11:50:42 -0300

    Remove whitespaces

--- TodoList/Extensions/String+Translate.swift
+++ TodoList/Extensions/String+Translate.swift
@@ -5,24 +5,18 @@ extension String {
     static let empty = ""
 
     struct Tasks {
-
         struct List {
             static let title = String.translate(key: "tasks.list.title")
         }
-
         struct Create {
             static let title = String.translate(key: "tasks.create.title")
-
             struct Message {
                 static let success = String.translate(key: "tasks.create.message.success")
             }
-
         }
-
         struct Label {
             static let title = String.translate(key: "tasks.create.label.title")
         }
-
     }
 
     static func translate(key: String, comment: String = .empty, _ args: CVarArg...) -> String {
